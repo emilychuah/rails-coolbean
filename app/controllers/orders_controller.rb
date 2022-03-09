8fc1298a89a686c30011ee2326b53e7195d3a58e
christopher baldwin <christopherbaldwin01@outlook.com>
2022-03-09 11:09:49 +1100

    meals orders stuff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,16 @@
 class OrdersController < ApplicationController
-  def show
 
+  def show
+    @order = Order.find(params[:id])
+    @order
   end
+
+  def index
+    @orders = Order.all
+  end
+  # private
+
+  # def find_order
+  #   @order = Order.find(params[:id])
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Order.create(is_paid: false, is_completed: false, created_at: 'Sun, 01 Jan 2012 00:00:00 +0300', payee_id: 1, payer_id: 2  )
