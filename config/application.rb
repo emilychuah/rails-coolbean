fb8c6d91099cc25a13b790b78be0072976f56611
Theresa Luo <theresaa.luo@gmail.com>
2022-03-14 17:54:22 +1100

    more features to the show page meals

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsCoolbean
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.exceptions_app = self.routes
   end
 end
