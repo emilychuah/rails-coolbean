c4d94dc7da7f7413ecc497f3516b804daacfdae4
Emily Chuah <emily_earn@live.com>
2022-03-17 12:03:26 +1100

    Changed data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,12 +65,12 @@ puts "Creating meals..."
 lasagne = Meal.create!(
   user: emily,
   name: "Lasagne",
-  price: 7,
+  price: 9,
   description: "100% homemade and healthy ingredients",
-  collect_from: (DateTime.now - 2.hour).strftime("%Y-%m-%d %I %p"),
-  collect_to: (DateTime.now + 2.day - 2.hour).strftime("%Y-%m-%d %I %p"),
+  collect_from: DateTime.parse("2022-03-18 11:00:00 +1100").strftime("%Y-%m-%d %I %p"),
+  collect_to: DateTime.parse("2022-03-18 14:00:00 +1100").strftime("%Y-%m-%d %I %p"),
   address: "31 Curzon St, West Melbourne VIC 3003",
-  available_quantity: 8,
+  available_quantity: 10,
   cuisine: "Italian",
   picture_url: "https://images.unsplash.com/photo-1619895092538-128341789043?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
 )
@@ -78,12 +78,12 @@ lasagne = Meal.create!(
 hamburger = Meal.create!(
   user: chris,
   name: "Hamburger",
-  price: 6,
+  price: 8,
   description: "Beef patty, wholemeal bread, cheese, lettuce, homemade aioli and tomatoes",
-  collect_from: (DateTime.now - 1.hour).strftime("%Y-%m-%d %I %p"),
-  collect_to: (DateTime.now + 3.day - 1.hour).strftime("%Y-%m-%d %I %p"),
+  collect_from: DateTime.parse("2022-03-18 17:00:00 +1100").strftime("%Y-%m-%d %I %p"),
+  collect_to: DateTime.parse("2022-03-18 19:00:00 +1100").strftime("%Y-%m-%d %I %p"),
   address: "2 Burt St, Northcote VIC 3070",
-  available_quantity: 20,
+  available_quantity: 15,
   cuisine: "Western",
   picture_url: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80"
 )
@@ -91,12 +91,12 @@ hamburger = Meal.create!(
 tiramisu = Meal.create!(
   user: sam,
   name: "Tiramisu",
-  price: 5,
+  price: 10,
   description: "Homemade tiramisu with a sweet, creamy, coffee flavour",
-  collect_from: DateTime.now.strftime("%Y-%m-%d %I %p"),
-  collect_to: (DateTime.now + 1.day).strftime("%Y-%m-%d %I %p"),
+  collect_from: DateTime.parse("2022-03-18 17:00:00 +1100").strftime("%Y-%m-%d %I %p"),
+  collect_to: DateTime.parse("2022-03-18 20:00:00 +1100").strftime("%Y-%m-%d %I %p"),
   address: "49 Sackville St, Kew VIC 3101",
-  available_quantity: 10,
+  available_quantity: 12,
   cuisine: "Italian",
   picture_url: "https://media.istockphoto.com/photos/portion-of-tiramisu-dessert-picture-id1061450296?b=1&k=20&m=1061450296&s=170667a&w=0&h=9OmAEu8aPV3Q1U_HwlFq1nHjrt1TQcII-wxS4HSCl_c="
 )
@@ -104,12 +104,12 @@ tiramisu = Meal.create!(
 laksa = Meal.create!(
   user: theresa,
   name: "Laksa",
-  price_cents: 950,
+  price_cents: 1250,
   description: "Gluten free and vegan! Guaranteed healthy and tasty!",
-  collect_from: (DateTime.now - 3.hour).strftime("%Y-%m-%d %I %p"),
-  collect_to: (DateTime.now + 2.day - 3.hour).strftime("%Y-%m-%d %I %p"),
+  collect_from: DateTime.parse("2022-03-18 12:00:00 +1100").strftime("%Y-%m-%d %I %p"),
+  collect_to: DateTime.parse("2022-03-18 15:00:00 +1100").strftime("%Y-%m-%d %I %p"),
   address: "450 Elizabeth Street, Melbourne VIC 3000",
-  available_quantity: 12,
+  available_quantity: 20,
   cuisine: "Asian",
   picture_url: "https://media.istockphoto.com/photos/singaporean-laksa-soup-picture-id907689936?b=1&k=20&m=907689936&s=170667a&w=0&h=cybLfOnIm_YZnLUeL7IoP9GV6D-Z0t1_he68vy-n9yM="
 )
